12d7be722e99990bf5d315982b1bbe74f6c418e1
benwilson <benjamin.wilson@databricks.com>
2018-11-05 15:24:30 -0500

    Finishing Pearson Filtering for feature importance culling

--- src/main/scala/com/databricks/spark/automatedml/PearsonFiltering.scala
+++ src/main/scala/com/databricks/spark/automatedml/PearsonFiltering.scala
@@ -1,59 +1,69 @@
 package com.databricks.spark.automatedml
 
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.ml.linalg.{Matrix, Vectors, Vector}
-import org.apache.spark.ml.stat.Correlation
-import org.apache.spark.sql.Row
+import org.apache.spark.sql.functions._
+import org.apache.spark.ml.linalg.Vector
 import org.apache.spark.ml.stat.ChiSquareTest
 import scala.collection.mutable.ListBuffer
 
-//TODO: finish this for dynamic and manual filtering of feature fields based on pearson relevance scores.
+/**
+  *
+  * @param df                   : DataFrame -> Dataset with a vectorized field of features,
+  *                             the feature columns, and a label column.
+  * @param featureColumnListing : Array[String] -> List of all fields that make up the feature vector
+  *
+  *                             Usage:
+  *                             val autoFiltered = new PearsonFiltering(featurizedData, fields)
+  *                             .setLabelCol("label")
+  *                             .setFeaturesCol("features")
+  *                             .setFilterStatistic("pearsonStat")
+  *                             .setFilterDirection("greater")
+  *                             .setFilterMode("auto")
+  *                             .setAutoFilterNTile(0.5)
+  *                             .filterFields
+  */
 
-
-class PearsonFiltering(df: DataFrame) extends DataValidation {
+class PearsonFiltering(df: DataFrame, featureColumnListing: Array[String]) extends DataValidation {
 
   private var _labelCol: String = "label"
   private var _featuresCol: String = "features"
-  private var _featureColumnsListing: Array[String] = Array.empty[String]
   private var _filterStatistic: String = "pearsonStat"
   private var _filterDirection: String = "greater"
 
   private var _filterManualValue: Double = 0.0
   private var _filterMode: String = "auto"
+  private var _autoFilterNTile: Double = 0.75
 
 
   final private val _dataFieldNames = df.schema.fieldNames
+  final private val _dataFieldTypes = df.schema.fields
   final private val _allowedStats: Array[String] = Array("pvalue", "degreesFreedom", "pearsonStat")
   final private val _allowedFilterDirections: Array[String] = Array("greater", "lesser")
   final private val _allowedFilterModes: Array[String] = Array("auto", "manual")
 
   def setLabelCol(value: String): this.type = {
-    assert(_dataFieldNames.contains(value), s"Label Field $value is not in DataFrame Schema.")
+    require(_dataFieldNames.contains(value), s"Label Field $value is not in DataFrame Schema.")
     _labelCol = value
     this
   }
 
   def setFeaturesCol(value: String): this.type = {
-    assert(_dataFieldNames.contains(value), s"Feature Field $value is not in DataFrame Schema.")
+    require(_dataFieldNames.contains(value), s"Feature Field $value is not in DataFrame Schema.")
+    require(_dataFieldTypes.filter(_.name == value)(0).dataType.typeName == "vector",
+      s"Feature Field $value is not of vector type.")
     _featuresCol = value
     this
   }
 
-  def setFeatureColumnsListing(value: Array[String]): this.type = {
-    assert(value.length > 0, s"Size of feature colums listing array must not be empty.")
-    _featureColumnsListing = value
-    this
-  }
-
   def setFilterStatistic(value: String): this.type = {
-    assert(_allowedStats.contains(value), s"Pearson Filtering Statistic '$value' is not a valid member of ${
+    require(_allowedStats.contains(value), s"Pearson Filtering Statistic '$value' is not a valid member of ${
       invalidateSelection(value, _allowedStats)}")
     _filterStatistic = value
     this
   }
 
   def setFilterDirection(value: String): this.type = {
-    assert(_allowedFilterDirections.contains(value), s"Filter Direction '$value' is not a valid member of ${
+    require(_allowedFilterDirections.contains(value), s"Filter Direction '$value' is not a valid member of ${
       invalidateSelection(value, _allowedFilterDirections)
     }")
     _filterDirection = value
@@ -65,23 +75,31 @@ class PearsonFiltering(df: DataFrame) extends DataValidation {
     this
   }
 
+  def setFilterManualValue(value: Int): this.type = {
+    _filterManualValue = value.toDouble
+    this
+  }
+
   def setFilterMode(value: String): this.type = {
-    assert(_allowedFilterModes.contains(value), s"Filter Mode $value is not a valid member of ${
+    require(_allowedFilterModes.contains(value), s"Filter Mode $value is not a valid member of ${
       invalidateSelection(value, _allowedFilterModes)}")
     _filterMode = value
     this
   }
 
-  //TODO: getters setters for filtering mode / manual value, provide quantile representation for automatic mode.
+  def setAutoFilterNTile(value: Double): this.type = {
+    require(value <= 1.0 & value >= 0.0, "NTile value must be between 0 and 1.")
+    _autoFilterNTile = value
+    this
+  }
 
   def getLabelCol: String = _labelCol
   def getFeaturesCol: String = _featuresCol
-  def getFeatureColumnsListing: Array[String] = _featureColumnsListing
   def getFilterStatistic: String = _filterStatistic
   def getFilterDirection: String = _filterDirection
   def getFilterManualValue: Double = _filterManualValue
   def getFilterMode: String = _filterMode
-
+  def getAutoFilterNTile: Double = _autoFilterNTile
 
   private def buildChiSq(): List[PearsonPayload] = {
     val reportBuffer = new ListBuffer[PearsonPayload]
@@ -91,13 +109,13 @@ class PearsonFiltering(df: DataFrame) extends DataValidation {
     val degreesFreedom = chi.getSeq[Int](1).toArray
     val pearsonStat = chi.getAs[Vector](2).toArray
 
-    for(i <- _featureColumnsListing.indices){
-      reportBuffer += PearsonPayload(_featureColumnsListing(i), pvalues(i), degreesFreedom(i), pearsonStat(i))
+    for(i <- featureColumnListing.indices){
+      reportBuffer += PearsonPayload(featureColumnListing(i), pvalues(i), degreesFreedom(i), pearsonStat(i))
     }
     reportBuffer.result
   }
 
-  def manualFilterChiSq(statPayload: List[PearsonPayload], filterValue: Double): List[String] = {
+  private def filterChiSq(statPayload: List[PearsonPayload], filterValue: Double): List[String] = {
     val fieldRestriction = new ListBuffer[String]
     _filterDirection match {
       case "greater" =>
@@ -126,21 +144,20 @@ class PearsonFiltering(df: DataFrame) extends DataValidation {
     fieldRestriction.result
   }
 
-  def quantileGenerator(percentile: Double, stat: String, pearsonResults: List[PearsonPayload]): Double = {
+  private def quantileGenerator(pearsonResults: List[PearsonPayload]): Double = {
 
-    assert(percentile < 1 & percentile > 0, "Percentile Value must be between 0 and 1.")
     val statBuffer = new ListBuffer[Double]
     pearsonResults.foreach(x => {
       x.getClass.getDeclaredFields foreach {f=>
         f.setAccessible(true)
-        if(f.getName == stat) statBuffer += f.get(x).asInstanceOf[Double]
+        if(f.getName == _filterStatistic) statBuffer += f.get(x).asInstanceOf[Double]
       }
     })
 
     val statSorted = statBuffer.result.sortWith(_<_)
-    if(statSorted.size % 2 == 1) statSorted((statSorted.size * percentile).toInt)
+    if(statSorted.size % 2 == 1) statSorted((statSorted.size * _autoFilterNTile).toInt)
     else {
-      val splitLoc = math.floor(statSorted.size * percentile).toInt
+      val splitLoc = math.floor(statSorted.size * _autoFilterNTile).toInt
       val splitCheck = if(splitLoc < 1) 1 else splitLoc.toInt
       val(high, low) = statSorted.splitAt(splitCheck)
       (high.last + low.head) / 2
@@ -148,6 +165,18 @@ class PearsonFiltering(df: DataFrame) extends DataValidation {
 
   }
 
-  //TODO: public accessor main method for doing either manual or automated filtering of the pearson data - returning a DataFrame.
+  def filterFields(): DataFrame = {
+
+    val chiSqData = buildChiSq()
+    val featureFields: List[String] = _filterMode match {
+      case "manual" =>
+        filterChiSq(chiSqData, _filterManualValue)
+      case _ =>
+        filterChiSq(chiSqData, quantileGenerator(chiSqData))
+      }
+    require(featureFields.nonEmpty, "All feature fields have been filtered out.  Adjust parameters.")
+    val fieldListing = featureFields ::: List(_labelCol)
+    df.select(fieldListing.map(col):_*)
+  }
 
 }
