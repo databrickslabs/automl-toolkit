de84ac0198af217ab68379f897418adcffc65155
benwilson <benjamin.wilson@databricks.com>
2018-11-07 18:24:55 -0500

    Split out common variables and methods into Trait to prep for Linear and Logistic Regression

--- src/main/scala/com/databricks/spark/automatedml/GeneticTuner.scala
+++ src/main/scala/com/databricks/spark/automatedml/GeneticTuner.scala
@@ -7,39 +7,21 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
 
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
-import scala.reflect.runtime.universe._
 
 //TODO: change the par mapping to proper thread pooling?
 //TODO: feature flag for logging to MLFlow, retain all the scoring and metrics.
 
-class GeneticTuner(df: DataFrame, modelSelection: String) extends DataValidation
-  with SparkSessionWrapper with Evolution {
+class GeneticTuner(df: DataFrame, modelSelection: String) extends SparkSessionWrapper
+  with Evolution {
 
   private var _scoringMetric = modelSelection match {
     case "regressor" => "rmse"
     case "classifier" => "f1"
     case _ => throw new UnsupportedOperationException(s"Model $modelSelection is not supported.")
   }
-  private var _optimizationStrategy = "maximize"
-  private var _firstGenerationGenePool = 20
-  private var _numberOfMutationGenerations = 10
-  private var _numberOfParentsToRetain = 3
-  private var _numberOfMutationsPerGeneration = 10
-  private var _geneticMixing = 0.7
-  private var _generationalMutationStrategy = "linear"
-  private var _mutationMagnitudeMode = "random"
-  private var _fixedMutationValue = 1
 
-
-  final val allowableStrategies = Seq("minimize", "maximize")
-  final val allowableMutationStrategies = Seq("linear", "fixed")
-  final val allowableMutationMagnitudeMode = Seq("random", "fixed")
-
-  final val classificationMetrics = List("f1", "weightedPrecision", "weightedRecall", "accuracy")
-  final val regressionMetrics = List("rmse", "mse", "r2", "mae")
-
-  private val _randomizer = scala.util.Random
-  _randomizer.setSeed(_seed)
+  final val classificationMetrics: List[String] = List("f1", "weightedPrecision", "weightedRecall", "accuracy")
+  final val regressionMetrics: List[String] = List("rmse", "mse", "r2", "mae")
 
   private var _randomForestNumericBoundaries = Map(
     "numTrees" -> Tuple2(50.0, 1000.0),
@@ -53,7 +35,6 @@ class GeneticTuner(df: DataFrame, modelSelection: String) extends DataValidation
     "impurity" -> List("gini", "entropy"),
     "featureSubsetStrategy" -> List("all", "sqrt", "log2", "onethird")
   )
-
 
   def setScoringMetric(value: String): this.type = {
     modelSelection match {
@@ -71,76 +52,6 @@ class GeneticTuner(df: DataFrame, modelSelection: String) extends DataValidation
     this
   }
 
-  def setOptimizationStrategy(value: String): this.type = {
-    val valueLC = value.toLowerCase
-    assert(allowableStrategies.contains(valueLC),
-      s"Optimization Strategy '$valueLC' is not a member of ${
-        invalidateSelection(valueLC, allowableStrategies)
-      }")
-    this._optimizationStrategy = valueLC
-    this
-  }
-
-  def setFirstGenerationGenePool(value: Int): this.type = {
-    assert(value > 5, s"Values less than 5 for firstGenerationGenePool will require excessive generational mutation to converge")
-    this._firstGenerationGenePool = value
-    this
-  }
-
-  def setNumberOfMutationGenerations(value: Int): this.type = {
-    assert(value > 0, s"Number of Generations must be greater than 0")
-    this._numberOfMutationGenerations = value
-    this
-  }
-
-  def setNumberOfParentsToRetain(value: Int): this.type = {
-    assert(value > 0, s"Number of Parents must be greater than 0. '$value' is not a valid number.")
-    this._numberOfParentsToRetain = value
-    this
-  }
-
-  def setNumberOfMutationsPerGeneration(value: Int): this.type = {
-    assert(value > 0, s"Number of Mutations per generation must be greater than 0. '$value' is not a valid number.")
-    this._numberOfMutationsPerGeneration = value
-    this
-  }
-
-  def setGeneticMixing(value: Double): this.type = {
-    assert(value < 1.0 & value > 0.0,
-      s"Mutation Aggressiveness must be in range (0,1). Current Setting of $value is not permitted.")
-    this._geneticMixing = value
-    this
-  }
-
-  def setGenerationalMutationStrategy(value: String): this.type = {
-    val valueLC = value.toLowerCase
-    assert(allowableMutationStrategies.contains(valueLC),
-      s"Generational Mutation Strategy '$valueLC' is not a member of ${
-        invalidateSelection(valueLC, allowableMutationStrategies)
-      }")
-    this._generationalMutationStrategy = valueLC
-    this
-  }
-
-  def setMutationMagnitudeMode(value: String): this.type = {
-    val valueLC = value.toLowerCase
-    assert(allowableMutationMagnitudeMode.contains(valueLC),
-      s"Mutation Magnitude Mode '$valueLC' is not a member of ${
-        invalidateSelection(valueLC, allowableMutationMagnitudeMode)
-      }")
-    this._mutationMagnitudeMode = valueLC
-    this
-  }
-
-  def setFixedMutationValue(value: Int): this.type = {
-    val maxMutationCount = modelConfigLength[RandomForestConfig]
-    assert(value <= maxMutationCount,
-      s"Mutation count '$value' cannot exceed number of hyperparameters ($maxMutationCount)")
-    assert(value > 0, s"Mutation count '$value' must be greater than 0")
-    this._fixedMutationValue = value
-    this
-  }
-
   def setRandomForestNumericBoundaries(value: Map[String, (Double, Double)]): this.type = {
     this._randomForestNumericBoundaries = value
     this
@@ -151,40 +62,15 @@ class GeneticTuner(df: DataFrame, modelSelection: String) extends DataValidation
     this
   }
 
-
   def getScoringMetric: String = _scoringMetric
-
-  def getOptimizationStrategy: String = _optimizationStrategy
-
-  def getFirstGenerationGenePool: Int = _firstGenerationGenePool
-
-  def getNumberOfMutationGenerations: Int = _numberOfMutationGenerations
-
-  def getNumberOfParentsToRetain: Int = _numberOfParentsToRetain
-
-  def getNumberOfMutationsPerGeneration: Int = _numberOfMutationsPerGeneration
-
-  def getGeneticMixing: Double = _geneticMixing
-
-  def getGenerationalMutationStrategy: String = _generationalMutationStrategy
-
-  def getMutationMagnitudeMode: String = _mutationMagnitudeMode
-
-  def getFixedMutationValue: Int = _fixedMutationValue
 
   def getRandomForestNumericBoundaries: Map[String, (Double, Double)] = _randomForestNumericBoundaries
 
   def getRandomForestStringBoundaries: Map[String, List[String]] = _randomForestStringBoundaries
 
-  private def modelConfigLength[T: TypeTag]: Int = {
-    typeOf[T].members.collect {
-      case m: MethodSymbol if m.isCaseAccessor => m
-    }.toList.length
-  }
+  def getClassificationMetrics: List[String] = classificationMetrics
 
-  private def genTestTrain(data: DataFrame, seed: Long): Array[DataFrame] = {
-    data.randomSplit(Array(_trainPortion, 1 - _trainPortion), seed)
-  }
+  def getRegressionMetrics: List[String] = regressionMetrics
 
   private def modelDecider[A, B](modelConfig: RandomForestConfig) = {
 
@@ -218,29 +104,7 @@ class GeneticTuner(df: DataFrame, modelSelection: String) extends DataValidation
     builtModel
   }
 
-  private def extractBoundaryDouble(param: String, boundaryMap: Map[String, (AnyVal, AnyVal)]): (Double, Double) = {
-    val minimum = boundaryMap(param)._1.asInstanceOf[Double]
-    val maximum = boundaryMap(param)._2.asInstanceOf[Double]
-    (minimum, maximum)
-  }
-
-  private def extractBoundaryInteger(param: String, boundaryMap: Map[String, (AnyVal, AnyVal)]): (Int, Int) = {
-    val minimum = boundaryMap(param)._1.asInstanceOf[Double].toInt
-    val maximum = boundaryMap(param)._2.asInstanceOf[Double].toInt
-    (minimum, maximum)
-  }
-
-  private def generateRandomDouble(param: String, boundaryMap: Map[String, (AnyVal, AnyVal)]): Double = {
-    val (minimumValue, maximumValue) = extractBoundaryDouble(param, boundaryMap)
-    (_randomizer.nextDouble * (maximumValue - minimumValue)) + minimumValue
-  }
-
-  private def generateRandomInteger(param: String, boundaryMap: Map[String, (AnyVal, AnyVal)]): Int = {
-    val (minimumValue, maximumValue) = extractBoundaryInteger(param, boundaryMap)
-    _randomizer.nextInt(maximumValue - minimumValue) + minimumValue
-  }
-
-  private def generateRandomString(param: String, boundaryMap: Map[String, List[String]]): String = {
+  override def generateRandomString(param: String, boundaryMap: Map[String, List[String]]): String = {
 
     val stringListing = param match {
       case "impurity" => modelSelection match {
@@ -357,56 +221,6 @@ class GeneticTuner(df: DataFrame, modelSelection: String) extends DataValidation
       case "minimize" => results.toArray.sortWith(_.score < _.score)
       case _ => results.toArray.sortWith(_.score > _.score)
     }
-  }
-
-  private def getRandomIndeces(minimum: Int, maximum: Int, parameterCount: Int): List[Int] = {
-    val fullIndexArray = List.range(0, maximum)
-    val randomSeed = new scala.util.Random
-    val count = minimum + randomSeed.nextInt((parameterCount - minimum) + 1)
-    val adjCount = if (count < 1) 1 else count
-    val shuffledArray = scala.util.Random.shuffle(fullIndexArray).take(adjCount)
-    shuffledArray.sortWith(_ < _)
-  }
-
-  private def getFixedIndeces(minimum: Int, maximum: Int, parameterCount: Int): List[Int] = {
-    val fullIndexArray = List.range(0, maximum)
-    val randomSeed = new scala.util.Random
-    randomSeed.shuffle(fullIndexArray).take(parameterCount).sortWith(_ < _)
-  }
-
-  private def generateMutationIndeces(minimum: Int, maximum: Int, parameterCount: Int,
-                                      mutationCount: Int): Array[List[Int]] = {
-    val mutations = new ArrayBuffer[List[Int]]
-    for (_ <- 0 to mutationCount) {
-      _mutationMagnitudeMode match {
-        case "random" => mutations += getRandomIndeces(minimum, maximum, parameterCount)
-        case "fixed" => mutations += getFixedIndeces(minimum, maximum, parameterCount)
-        case _ => new UnsupportedOperationException(s"Unsupported mutationMagnitudeMode ${_mutationMagnitudeMode}")
-      }
-    }
-    mutations.result.toArray
-  }
-
-  private def generateIdealParents(results: Array[RandomForestModelsWithResults]): Array[RandomForestConfig] = {
-    val bestParents = new ArrayBuffer[RandomForestConfig]
-    results.take(_numberOfParentsToRetain).map(x => {
-      bestParents += x.modelHyperParams
-    })
-    bestParents.result.toArray
-  }
-
-  private def geneMixing(parent: Double, child: Double, parentMutationPercentage: Double): Double = {
-    (parent * parentMutationPercentage) + (child * (1 - parentMutationPercentage))
-  }
-
-  private def geneMixing(parent: Int, child: Int, parentMutationPercentage: Double): Int = {
-    ((parent * parentMutationPercentage) + (child * (1 - parentMutationPercentage))).toInt
-  }
-
-  private def geneMixing(parent: String, child: String): String = {
-    val mixed = new ArrayBuffer[String]
-    mixed += parent += child
-    scala.util.Random.shuffle(mixed.toList).head
   }
 
   def irradiateGeneration(parents: Array[RandomForestConfig], mutationCount: Int,
