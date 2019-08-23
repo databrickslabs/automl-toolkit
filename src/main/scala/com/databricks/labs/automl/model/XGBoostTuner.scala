b615c2b13537c4f6f74d1921dfc1c9d11671588a
benwilson <benjamin.wilson@databricks.com>
2019-08-23 18:55:31 -0400

    XGBoost critical fix for classifiers HOTFIX

--- src/main/scala/com/databricks/labs/automl/model/XGBoostTuner.scala
+++ src/main/scala/com/databricks/labs/automl/model/XGBoostTuner.scala
@@ -124,6 +124,7 @@ class XGBoostTuner(df: DataFrame, modelSelection: String)
           .setMinChildWeight(modelConfig.minChildWeight)
           .setNumRound(modelConfig.numRound)
           .setTrainTestRatio(modelConfig.trainTestRatio)
+          .setMissing(0)
         if (uniqueLabels > 2) {
           xgClass
             .setNumClass(uniqueLabels)
