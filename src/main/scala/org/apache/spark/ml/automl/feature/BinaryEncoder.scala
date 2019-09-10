203ea957b7114453d975c4db4d814f67e45cb7a4
benwilson <benjamin.wilson@databricks.com>
2019-09-09 20:58:22 -0400

    Finished scaladocs

--- src/main/scala/org/apache/spark/ml/automl/feature/BinaryEncoder.scala
+++ src/main/scala/org/apache/spark/ml/automl/feature/BinaryEncoder.scala
@@ -53,6 +53,7 @@ trait BinaryEncoderBase
     *                                       length.
     * @author Ben Wilson, Databricks
     */
+  @throws(classOf[UnsupportedOperationException])
   protected def validateAndTransformSchema(schema: StructType,
                                            keepInvalid: Boolean): StructType = {
 
@@ -122,6 +123,7 @@ class BinaryEncoder(override val uid: String)
     * @since 0.5.3
     * @author Ben Wilson, Databricks
     */
+  @throws(classOf[SparkException])
   def setHandleInvalid(value: String): this.type = set(handleInvalid, value)
 
   override def transformSchema(schema: StructType): StructType = {
@@ -135,9 +137,22 @@ class BinaryEncoder(override val uid: String)
     * The primary principle at work here is dimensionality reduction for the encoding of extremely high-cardinality
     * StringIndexed columns.  OneHotEncoding works extremely well for this purpose, but has the side-effect of
     * requiring extremely large amounts of columns to be generated when performing OHE is increased memory pressure.
-    * This package allows for a lossy reduction
-    * @param dataset
-    * @return
+    * This package allows for a lossy reduction in this space by distilling the information into a binary string
+    * encoding space that is dynamic based on the encoded length of the maximum nominal space as represented in binary
+    *
+    * @example e.g. if the cardinality of a nominal column is 113, the binary representation of that is 1110001.
+    *          When using OHE, this would result in 113 (or 114 if allowing invalids) binary positions within a sparse
+    *          vector, creating 113 or 114 columns in the dataset.  However, using BinaryEncoder, we are left with 7
+    *          (or 8, if allowing invalids) dense vector positions to capture the same amount of information.
+    *
+    * @note Due to the nature of this encoding and how the majority of models learn, this is seen as an information
+    *       loss encoding.  However, considering that high cardinality non-numeric nominal fields are frequently
+    *       discarded due to the explosion of the data set, this is providing the ability to utilize high cardinality
+    *       fields that otherwise would not be able to be included.
+    * @param dataset The dataset (or DataFrame) used in training the model
+    * @return BinaryEncoderModel - a serializable artifact that has the output schema and encoding embedded within it.
+    * @since 0.5.3
+    * @author Ben Wilson, Databricks
     */
   override def fit(dataset: Dataset[_]): BinaryEncoderModel = {
 
@@ -207,6 +222,11 @@ class BinaryEncoderModel(override val uid: String,
 
   import BinaryEncoderModel._
 
+  /**
+    * Helper method for adjusting the sizes that the Binary Encoder will need to encode to capture the BinaryString
+    * representation of the Nominal data, adjusted for the keepInvalid flag.
+    * @since 0.5.3
+    */
   private def getConfigedCategorySizes: Array[Int] = {
 
     val keepInvalid = getHandleInvalid == BinaryEncoder.KEEP_INVALID
@@ -219,6 +239,15 @@ class BinaryEncoderModel(override val uid: String,
 
   }
 
+  /**
+    * Main UDF for performing the conversion of nominal / binary encoded data from StringIndexer to a BinaryString format
+    * as a Breeze DenseVector
+    * @throws SparkException if the value for encoding is not within the column index during training
+    *                        (keepInvalid set to 'error')
+    * @since 0.5.3
+    * @author Ben Wilson, Databricks
+    */
+  @throws(classOf[SparkException])
   private def encoder: UserDefinedFunction = {
 
     val keepInvalid = getHandleInvalid == BinaryEncoder.KEEP_INVALID
@@ -255,10 +284,41 @@ class BinaryEncoderModel(override val uid: String,
     }
   }
 
+  /**
+    * Setter for specifying the column names in Array format for the columns intended to be Binary Indexed.
+    * @param values Array of column names
+    * @since 0.5.3
+    * @author Ben Wilson, DataBricks
+    */
   def setInputCols(values: Array[String]): this.type = set(inputCols, values)
+
+  /**
+    * Setter for specifying the desired output columns in Array format for the columns to be generated as Breeze
+    * DenseVectors when the model is used to transform a dataset
+    * @param values Array of output column names
+    * @note the index position relationship between setInputCols and setOutputCols is a 1 to 1 relationship.  The
+    *       positional order and length must be congruent and match.
+    * @since 0.5.3
+    * @author Ben Wilson, Databricks
+    */
   def setOutputCols(values: Array[String]): this.type = set(outputCols, values)
+
+  /**
+    * Setter for whether to allow for unseen indexed nominal values to be used in the transformation of a dataset with
+    * the generated BinaryEncoderModel.
+    * @note Default: 'error' optional settings: 'keep' or 'error'
+    * @param value The setting to be used: either 'keep' or 'error'
+    * @since 0.5.3
+    * @author Ben Wilson, Databricks
+    */
   def setHandleInvalid(value: String): this.type = set(handleInvalid, value)
 
+  /**
+    * Method for mutating the dataset schema to support the addition of BinaryEncoded columns
+    * @param schema the schema of the dataset
+    * @since 0.5.3
+    * @author Ben Wilson, Databricks
+    */
   override def transformSchema(schema: StructType): StructType = {
 
     val inputColNames = $(inputCols)
@@ -277,6 +337,15 @@ class BinaryEncoderModel(override val uid: String,
 
   }
 
+  /**
+    * Private method for validating that the schema metadata matches the expected cardinality for the column to be encoded
+    * @param schema schema of the input dataset
+    * @throws IllegalArgumentException if the metadata information does not match the data cardinality
+    * @return the schema of the dataset (pass-through method)
+    * @since 0.5.3
+    * @author Ben Wilson, Databricks
+    */
+  @throws(classOf[IllegalArgumentException])
   private def verifyNumOfValues(schema: StructType): StructType = {
     val configedSizes = getConfigedCategorySizes
     $(outputCols).zipWithIndex.foreach {
@@ -296,6 +365,14 @@ class BinaryEncoderModel(override val uid: String,
     schema
   }
 
+  /**
+    * Main transformation method that will apply the model's configured encoding through a udf to the input dataset
+    * and add encoded columns.
+    * @param dataset input dataset for the model to mutate
+    * @return a DataFrame with added BinaryEncoded columns
+    * @since 0.5.3
+    * @author Ben Wilson, Databricks
+    */
   override def transform(dataset: Dataset[_]): DataFrame = {
 
     val transformedSchema = transformSchema(dataset.schema, logging = true)
@@ -382,8 +459,11 @@ private[feature] object BinaryEncoderCommon {
     * Helper method for appropriately padding leading zero's based on the BinaryString Length value and the
     * target encoding length
     * @param inputString A supplied BinaryString encoded value
+    * @throws IllegalArgumentException if the target encoding size is smaller than the data size after encoding,
+    *                                  since this would result in significant data loss and issues with modeling.
     * @return Padded string to the prescribed encoding length
     */
+  @throws(classOf[IllegalArgumentException])
   private[feature] def padZeros(inputString: String,
                                 encodingSize: Int): String = {
 
@@ -402,6 +482,16 @@ private[feature] object BinaryEncoderCommon {
 
   }
 
+  /**
+    * Private encoding method.
+    * @param value takes in an Option of Any type and casts it to a BinaryString representation
+    * @note Conversions from non-whole-number values of numeric types WILL incur information loss as decimal values
+    *       cannot be represented by simple BinaryString.  It will incur a rounding to the nearest whole number.
+    * @tparam A Any type
+    * @return Binary String conversion of the input data
+    * @throws UnsupportedOperationException if the data type being input is not of the correct type for conversion.
+    */
+  @throws(classOf[UnsupportedOperationException])
   private[feature] def convertToBinaryString[A <: Any](
     value: Option[A]
   ): String = {
@@ -428,6 +518,16 @@ private[feature] object BinaryEncoderCommon {
 
   }
 
+  /**
+    * Private method for converting Any type to and Array of Doubles with appropriate prefix padding to ensure
+    * constant vector length of the output DenseVector from transformation.
+    * @param ordinalValue The value to encode
+    * @param encodingSize The size of the Array of Binary Double values for output
+    * @tparam A Any type (primitives are only supported - collections will throw an exception)
+    * @return Array of Doubles, representing the Binary values of the encoded value
+    * @since 0.5.3
+    * @author Ben Wilson, Databricks
+    */
   private[feature] def convertToBinary[A <: Any](
     ordinalValue: Option[A],
     encodingSize: Int
@@ -438,6 +538,13 @@ private[feature] object BinaryEncoderCommon {
     binaryStringToDoubleArray(padded)
   }
 
+  /**
+    * Private method for converting a binary string to Array[Double]
+    * @param binary Binary Encoded String
+    * @return Array[Double]  for the numeric vector-representation of an encoded value in Binary format
+    * @since 0.5.3
+    * @author Ben Wilson, Databricks
+    */
   private[feature] def binaryStringToDoubleArray(
     binary: String
   ): Array[Double] = {
@@ -446,6 +553,14 @@ private[feature] object BinaryEncoderCommon {
 
   }
 
+  /**
+    * Pulled from OneHotEncoder with slight modifications.
+    * @param dataset Input dataset
+    * @param inputColNames The names that are to be encoded
+    * @param outputColNames the output columns after encoding
+    * @return Seq[AttributeGroup] for the schema metadata associated with the Model
+    * @since 0.5.3
+    */
   def getOutputAttrGroupFromData(
     dataset: Dataset[_],
     inputColNames: Seq[String],
@@ -501,6 +616,15 @@ private[feature] object BinaryEncoderCommon {
 
   }
 
+  /**
+    * Pulled from OneHotEncoder with slight modifications.
+    * @param outputColName Output Column to be created by the BinaryEncoderModel
+    * @param numAttrs The number of unique values for the output column
+    * @param keepInvalid Boolean flag for whether to allow for unseen values to be grouped into an unknown binary
+    *                    representation during transformation
+    * @return AttributeGroup
+    * @since 0.5.3
+    */
   def createAttrGroupForAttrNames(outputColName: String,
                                   numAttrs: Int,
                                   keepInvalid: Boolean): AttributeGroup = {
@@ -516,6 +640,13 @@ private[feature] object BinaryEncoderCommon {
     genOutputAttrGroup(Some(outputAttrNames), outputColName)
   }
 
+  /**
+    * Pulled from OneHotEncoder with slight modifications.
+    * @param outputAttrNames Attributes for the metadata
+    * @param outputColName The name of the output column to be added
+    * @return AttributeGroup
+    * @since 0.5.3
+    */
   private def genOutputAttrGroup(outputAttrNames: Option[Array[String]],
                                  outputColName: String): AttributeGroup = {
     outputAttrNames
@@ -530,6 +661,11 @@ private[feature] object BinaryEncoderCommon {
       }
   }
 
+  /**
+    * Pulled from OneHotEncoder with slight modifications.
+    * @param inputCol An input column to extract the encoding lengths for the output column during transformation
+    * @return Output names for attritubtes
+    */
   private def genOutputAttrNames(
     inputCol: StructField
   ): Option[Array[String]] = {
@@ -557,6 +693,13 @@ private[feature] object BinaryEncoderCommon {
     }
   }
 
+  /**
+    * Pulled from OneHotEncoder with slight modifications.
+    * @param inputCol An input column that will be used to validate the output column corresponding to it.
+    * @param outputColName An output column to match to the input column
+    * @param keepInvalid invalid retention flag
+    * @return the Output column struct field definition
+    */
   def transformOutputColumnSchema(inputCol: StructField,
                                   outputColName: String,
                                   keepInvalid: Boolean = false): StructField = {
