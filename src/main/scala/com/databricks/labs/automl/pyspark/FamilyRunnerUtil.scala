a4eb9c6e961b756de59e23c39390d7b792cfe53c
marygracemoesta <marygrace.moesta@databricks.com>
2019-11-16 20:28:30 -0500

    runner and FI utils

--- src/main/scala/com/databricks/labs/automl/pyspark/FamilyRunnerUtil.scala
+++ src/main/scala/com/databricks/labs/automl/pyspark/FamilyRunnerUtil.scala
@@ -1,5 +1,43 @@
 package com.databricks.labs.automl.pyspark
 
-object familyRunnerUtil {
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import org.apache.spark.sql.DataFrame
+import com.databricks.labs.automl.executor.config.{ConfigurationGenerator, InstanceConfig}
+import com.databricks.labs.automl.executor.FamilyRunner
+import org.apache.spark.sql.SparkSession
+
+object FamilyRunnerUtil {
+  lazy val objectMapper = new ObjectMapper()
+  def runFamilyRunner(configs:String,
+                      predictionType: String,
+                      df: DataFrame): Unit = {
+
+    val firstMap = jsonToMap(configs)
+    val familyRunnerConfigs = buildArray(firstMap,
+      predictionType)
+    //run the family runner
+    val runner = FamilyRunner(df, familyRunnerConfigs).executeWithPipeline()
+//    runner.bestMlFlowRunId.toSeq.toDF("modelFamily", "bestMlFlowRunId").createOrReplaceTempView("bestMlFlowRunId")
+    runner.familyFinalOutput.modelReportDataFrame.createOrReplaceTempView("modelReportDataFrame")
+    runner.familyFinalOutput.generationReportDataFrame.createOrReplaceTempView("generationReportDataFrame")
+  }
+
+  def buildArray(configs: Map[String, Any],
+                 predictionType: String): Array[InstanceConfig] = {
+
+    configs
+      .asInstanceOf[Map[String, Map[String, Any]]]
+      .map({
+        case (key, valuesMap) => {
+          ConfigurationGenerator.generateConfigFromMap(key, predictionType, valuesMap)
+        }
+      })
+      .toArray
+  }
+  def jsonToMap(message: String): Map[String, Any] = {
+    objectMapper.registerModule(DefaultScalaModule)
+    objectMapper.readValue(message, classOf[Map[String, Any]])
+  }
 
 }
--- src/main/scala/com/databricks/labs/automl/pyspark/FeatureImportanceUtil.scala
+++ src/main/scala/com/databricks/labs/automl/pyspark/FeatureImportanceUtil.scala
@@ -1,5 +1,61 @@
 package com.databricks.labs.automl.pyspark
 
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import org.apache.spark.ml.PipelineModel
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.DataFrame
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.databricks.labs.automl.executor.config.{ConfigurationGenerator, InstanceConfig}
+import org.apache.spark.sql.SparkSession
+import com.databricks.labs.automl.exploration.FeatureImportances
+
 object FeatureImportanceUtil {
+  lazy val objectMapper = new ObjectMapper()
+
+  def runFeatureImportance(modelFamily: String,
+                           predictionType: String,
+                           configJson: String,
+                           df: DataFrame,
+                           cutoffType: String,
+                           cutoffValue: Float,
+                           defaultFlag: String): Unit = {
+
+
+    val fiConfig  = defaultConfigFlag(defaultFlag,
+      configJson,
+      modelFamily,
+      predictionType)
+    //     val fiConfig = ConfigurationGenerator.generateConfigFromMap(modelFamily,predictionType,overrides)
+
+    val mainConfig = ConfigurationGenerator.generateFeatureImportanceConfig(fiConfig)
+    val FiImportances = FeatureImportances(df, mainConfig, cutoffType, cutoffValue).generateFeatureImportances()
+
+    //cache importances df and top fields to get them later in python
+    FiImportances.importances.createOrReplaceTempView("importances")
+    //     Seq(FiImportances.topFields).toDF("testCol").cache()
+
+  }
+
+  def defaultConfigFlag(defaultFlag: String,
+                        configJson: String,
+                        modelFamily: String,
+                        predictionType: String): InstanceConfig = {
+    if (defaultFlag == "true"){
+      // Generate default config if default flag is true
+      val fiConfig = ConfigurationGenerator.generateDefaultConfig(modelFamily, predictionType)
+      return fiConfig
+    }
+    else{
+      // Generating config from the map of overrides if default configs aren't being used
+      val overrides = jsonToMap(configJson)
+      val fiConfig = ConfigurationGenerator.generateConfigFromMap(modelFamily,predictionType,overrides)
+      return fiConfig
+    }
+  }
+
+  def jsonToMap(message: String): Map[String, Any] = {
+    objectMapper.registerModule(DefaultScalaModule)
+    objectMapper.readValue(message, classOf[Map[String, Any]])
+  }
 
 }
