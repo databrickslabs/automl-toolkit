a41351d047486a5668b717d2af26fd96e73c95e0
benwilson <benjamin.wilson@databricks.com>
2019-11-19 16:45:43 -0500

    Main first-pass logic complete - Entropy calculator for base features - Variance calculator for base features - Information Gain calculator for classification tasks - Feature Interaction generator - Scoring and evaluator - Operators for keeping/rejecting candidate interactions - Generator and collections for Pipelines created

--- src/main/scala/com/databricks/labs/automl/feature/FeatureInteraction.scala
+++ src/main/scala/com/databricks/labs/automl/feature/FeatureInteraction.scala
@@ -3,10 +3,16 @@ package com.databricks.labs.automl.feature
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
 
-class FeatureInteraction(modelingType: String) extends FeatureInteractionBase {
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.parallel.ForkJoinTaskSupport
+import scala.concurrent.forkjoin.ForkJoinPool
+
+class FeatureInteraction(modelingType: String, retentionMode: String)
+    extends FeatureInteractionBase {
 
   import ModelingType._
   import FieldEncodingType._
+  import InteractionRetentionMode._
 
   private var _labelCol: String = "label"
 
@@ -15,6 +21,10 @@ class FeatureInteraction(modelingType: String) extends FeatureInteractionBase {
   private var _fullDataVariance: Double = 0.0
 
   private var _continuousDiscretizerBucketCount: Int = 10
+
+  private var _parallelism: Int = 4
+
+  private var _targetInteractionPercentage: Double = 25.0
 
   def setLabelCol(value: String): this.type = {
     _labelCol = value
@@ -31,25 +41,22 @@ class FeatureInteraction(modelingType: String) extends FeatureInteractionBase {
     this
   }
 
-  /**
-    * Private method for generating a collection of Interaction Candidates to be tested and applied to the feature set
-    * if the tests for inclusion pass.
-    * @param featureColumns List of the columns that make up the feature vector
-    * @return Array of InteractionPayload values.
-    * @since 0.7.0
-    * @author Ben Wilson, Databricks
-    */
-  private def generateInteractionCandidates(
-    featureColumns: Array[String]
-  ): Array[InteractionPayload] = {
-    val colIdx = featureColumns.zipWithIndex
-    colIdx.flatMap {
-      case (x, i) =>
-        val maxIdx = colIdx.length
-        for (j <- Range(i + 1, maxIdx)) yield {
-          InteractionPayload(x, colIdx(j)._1, s"i_${x}_${colIdx(j)._1}")
-        }
-    }
+  def setParallelism(value: Int): this.type = {
+    require(
+      value > 0,
+      s"Parallelism value $value is invalid.  Must be 1 or greater."
+    )
+    _parallelism = value
+    this
+  }
+
+  def setTargetInteractionPercentage(value: Double): this.type = {
+    require(
+      value > 0,
+      s"Target Percentage allowance for inclusion must be a positive Double. $value is invalid."
+    )
+    _targetInteractionPercentage = value
+    this
   }
 
   /**
@@ -156,9 +163,96 @@ class FeatureInteraction(modelingType: String) extends FeatureInteractionBase {
 
   }
 
-  def initialize(df: DataFrame,
-                 nominalFields: Array[String],
-                 continuousFields: Array[String]) = {
+  /**
+    * Private method for evaluating an interacted column
+    * @param df A DataFrame to be used for candidate feature interaction evaluations
+    * @param candidate The InteractionPayload for the parents of left/right to make the interacted feature
+    * @param totalRecordCount Total number of records in the DataFrame (calculated only once for the Object)
+    * @return InteractionResult payload of interaction scores associated with the interacted features
+    * @since 0.7.0
+    * @author Ben Wilson, Databricks
+    */
+  private def evaluateInteraction(df: DataFrame,
+                                  candidate: InteractionPayload,
+                                  totalRecordCount: Long): InteractionResult = {
+
+    // Generate a subset DataFrame, create the interaction column, and retain only the fields needed.
+    val evaluationDf = df
+      .select(candidate.left, candidate.right, _labelCol)
+
+    val interactedDf = interactProduct(evaluationDf, candidate)
+
+    // Score the interaction
+    val score = scoreColumn(
+      interactedDf,
+      getModelType(modelingType),
+      candidate.outputName,
+      getFieldType("continuous"),
+      totalRecordCount
+    )
+
+    InteractionResult(
+      candidate.left,
+      candidate.right,
+      candidate.outputName,
+      score
+    )
+
+  }
+
+  /**
+    * Private method for comparing the parents scores to the interacted feature score and return a Boolean keep / not keep for the interacted feature in the final
+    * data set and configuration for this module.
+    * @param interactionResult the evaluated result of an interacted feature
+    * @param leftScore left parent of interaction's score
+    * @param rightScore right parent of interaction's score
+    * @return Boolean value of whether to keep the interacted field or not
+    * @since 0.7.0
+    * @author Ben Wilson, Databricks
+    */
+  private def parentCompare(interactionResult: InteractionResult,
+                            leftScore: Double,
+                            rightScore: Double): Boolean = {
+
+    val percentageChangeLeft =
+      calculatePercentageChange(leftScore, interactionResult.score)
+
+    val percentageChangeRight =
+      calculatePercentageChange(rightScore, interactionResult.score)
+
+    getRetentionMode(retentionMode) match {
+      case Optimistic =>
+        getModelType(modelingType) match {
+          case Regressor =>
+            percentageChangeLeft <= _targetInteractionPercentage | percentageChangeRight <= _targetInteractionPercentage
+          case Classifier =>
+            percentageChangeLeft >= -1 * _targetInteractionPercentage | percentageChangeRight >= -1 * _targetInteractionPercentage
+        }
+      case Strict =>
+        getModelType(modelingType) match {
+          case Regressor =>
+            percentageChangeLeft <= _targetInteractionPercentage & percentageChangeRight <= _targetInteractionPercentage
+          case Classifier =>
+            percentageChangeLeft >= -1 * _targetInteractionPercentage & percentageChangeRight >= -1 * _targetInteractionPercentage
+        }
+    }
+
+  }
+
+  /**
+    * Main method for generating a list of interaction candidates based on the configuration specified in the class configuration.
+    * @param df The DataFrame to process interactions for
+    * @param nominalFields The nominal fields (String Indexed) to be used for interaction
+    * @param continuousFields The continuous fields (Original Numeric Types) to be used for interaction
+    * @return Array[InteractionPayload] for candidate fields interactions that meet the acceptance criteria as set by configuration.
+    * @since 0.7.0
+    * @author Ben Wilson, Databricks
+    */
+  def generateCandidates(
+    df: DataFrame,
+    nominalFields: Array[String],
+    continuousFields: Array[String]
+  ): Array[InteractionPayload] = {
 
     val modelType = getModelType(modelingType)
 
@@ -192,11 +286,65 @@ class FeatureInteraction(modelingType: String) extends FeatureInteractionBase {
 
     val mergedParentScores = nominalScores ++ continuousScores
 
-    //TODO:
+    val interactionCandidates = generateInteractionCandidates(
+      nominalFields ++ continuousFields
+    )
 
-    // do interaction mapping and add fields
+    val forkJoinTaskSupport = new ForkJoinTaskSupport(
+      new ForkJoinPool(_parallelism)
+    )
+    val candidateChecks = interactionCandidates.par
+    candidateChecks.tasksupport = forkJoinTaskSupport
 
-    // compare fields to parents, drop if below / above threshold as configured.
+    val scoredCandidates = candidateChecks.map { x =>
+      evaluateInteraction(df, x, totalRecordCount)
+    }.toArray
+
+    var interactionBuffer = ArrayBuffer[InteractionPayload]()
+
+    // Iterate over the evaluations and determine whether to keep them
+
+    for (x <- scoredCandidates) {
+
+      if (parentCompare(
+            x,
+            mergedParentScores(x.left),
+            mergedParentScores(x.right)
+          ))
+        interactionBuffer += InteractionPayload(x.left, x.right, x.interaction)
+
+    }
+
+    interactionBuffer.toArray
+
+  }
+
+  /**
+    * Method for determining feature interaction candidates, apply those candidates as new fields to the DataFrame,
+    * and return a configuration payload that has the information about the interactions that can be used in a Pipeline.
+    * @param df DataFrame to be used to calculate and potentially add feature interactions to
+    * @param nominalFields Fields from the DataFrame that were originally non-numeric (Character, String, etc.)
+    * @param continuousFields Fields from the DataFrame that were originally numeric, continuous types.
+    * @return FeatureInteractionCollection -> the DataFrame with candidate feature interactions added in and the
+    *         payload of interaction features and their constituent parents in order to recreate for a Pipeline.
+    * @since 0.7.0
+    * @author Ben Wilson, Databricks
+    */
+  def createCandidates(
+    df: DataFrame,
+    nominalFields: Array[String],
+    continuousFields: Array[String]
+  ): FeatureInteractionCollection = {
+
+    val fieldsToCreate = generateCandidates(df, nominalFields, continuousFields)
+
+    var data = df
+
+    for (c <- fieldsToCreate) {
+      data = interactProduct(data, c)
+    }
+
+    FeatureInteractionCollection(data, fieldsToCreate)
 
   }
 
--- src/main/scala/com/databricks/labs/automl/feature/FeatureInteractionBase.scala
+++ src/main/scala/com/databricks/labs/automl/feature/FeatureInteractionBase.scala
@@ -1,13 +1,17 @@
 package com.databricks.labs.automl.feature
 
 import com.databricks.labs.automl.exceptions.ModelingTypeException
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.col
 
 trait FeatureInteractionBase {
   import ModelingType._
   import FieldEncodingType._
+  import InteractionRetentionMode._
 
   private final val allowableModelTypes = Array("classifier", "regressor")
   private final val allowableFieldTypes = Array("nominal", "continuous")
+  private final val allowableRetentionModes = Array("optimistic", "strict")
 
   final val AGGREGATE_COLUMN: String = "totalCount"
   final val COUNT_COLUMN: String = "count"
@@ -39,11 +43,86 @@ trait FeatureInteractionBase {
     }
   }
 
+  protected[feature] def getRetentionMode(
+    retentionMode: String
+  ): InteractionRetentionMode.Value = {
+    retentionMode match {
+      case "optimistic" => Optimistic
+      case "strict"     => Strict
+      case _ =>
+        throw ModelingTypeException(retentionMode, allowableRetentionModes)
+    }
+  }
+
+  /**
+    * Method for generating a collection of Interaction Candidates to be tested and applied to the feature set
+    * if the tests for inclusion pass.
+    * @param featureColumns List of the columns that make up the feature vector
+    * @return Array of InteractionPayload values.
+    * @since 0.7.0
+    * @author Ben Wilson, Databricks
+    */
+  protected[feature] def generateInteractionCandidates(
+    featureColumns: Array[String]
+  ): Array[InteractionPayload] = {
+    val colIdx = featureColumns.zipWithIndex
+    colIdx.flatMap {
+      case (x, i) =>
+        val maxIdx = colIdx.length
+        for (j <- Range(i + 1, maxIdx)) yield {
+          InteractionPayload(x, colIdx(j)._1, s"i_${x}_${colIdx(j)._1}")
+        }
+    }
+  }
+
+  /**
+    * Method for evaluating the percentage change to the score metric to normalize.
+    * @param before Score of a parent feature
+    * @param after Score of an interaction feature
+    * @return the percentage change
+    * @since 0.7.0
+    * @author Ben Wilson, Databricks
+    */
+  protected[feature] def calculatePercentageChange(before: Double,
+                                                   after: Double): Double = {
+
+    (after - before) / math.abs(before) * 100.0
+
+  }
+
+  /**
+    * Method for generating a product interaction between feature columns
+    * @param df A DataFrame to add a field for an interaction between two columns
+    * @param candidate InteractionPayload information about the two parent columns and the name of the new interaction column to be created.
+    * @return A modified DataFrame with the new column.
+    * @since 0.7.0
+    * @author Ben Wilson, Databricks
+    */
+  protected[feature] def interactProduct(
+    df: DataFrame,
+    candidate: InteractionPayload
+  ): DataFrame = {
+
+    df.withColumn(
+      candidate.outputName,
+      col(candidate.left) * col(candidate.right)
+    )
+
+  }
+
 }
 
 case class VarianceData(labelValue: Double, variance: Double)
 case class EntropyData(labelValue: Double, entropy: Double)
 case class InteractionPayload(left: String, right: String, outputName: String)
+case class InteractionResult(left: String,
+                             right: String,
+                             interaction: String,
+                             score: Double)
+case class FeatureInteractionCollection(
+  data: DataFrame,
+  interactionPayload: Array[InteractionPayload]
+)
 
 object ModelingType extends Enumeration {
   val Regressor = ModelType("regressor")
@@ -57,4 +136,12 @@ object FieldEncodingType extends Enumeration {
   val Continuous = FieldType("continuous")
   protected case class FieldType(fieldType: String) extends super.Val()
   implicit def convert(value: Value): FieldType = value.asInstanceOf[FieldType]
+}
+
+object InteractionRetentionMode extends Enumeration {
+  val Optimistic = RetentionMode("optimistic")
+  val Strict = RetentionMode("strict")
+  protected case class RetentionMode(retentionMode: String) extends super.Val()
+  implicit def convert(value: Value): RetentionMode =
+    value.asInstanceOf[RetentionMode]
 }
