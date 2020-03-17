9a202b7b711ff2532377ab7cbc5f229d524cf2d1
marygracemoesta <marygrace.moesta@databricks.com>
2020-03-17 14:20:39 -0400

    inference support

--- src/main/scala/com/databricks/labs/automl/pyspark/FamilyRunnerUtil.scala
+++ src/main/scala/com/databricks/labs/automl/pyspark/FamilyRunnerUtil.scala
@@ -6,12 +6,14 @@ import org.apache.spark.sql.DataFrame
 import com.databricks.labs.automl.executor.config.{ConfigurationGenerator, InstanceConfig}
 import com.databricks.labs.automl.executor.FamilyRunner
 import com.databricks.labs.automl.utils.SparkSessionWrapper
+import com.databricks.labs.automl.pipeline.inference.PipelineModelInference
 
 object FamilyRunnerUtil extends SparkSessionWrapper {
   lazy val objectMapper = new ObjectMapper()
   def runFamilyRunner(configs:String,
                       predictionType: String,
-                      df: DataFrame): Unit = {
+                      df: DataFrame,
+                      tmpPath: String): Unit = {
     import spark.implicits._
 
     val firstMap = jsonToMap(configs)
@@ -22,6 +24,35 @@ object FamilyRunnerUtil extends SparkSessionWrapper {
     runner.familyFinalOutput.modelReportDataFrame.createOrReplaceTempView("modelReportDataFrame")
     runner.familyFinalOutput.generationReportDataFrame.createOrReplaceTempView("generationReportDataFrame")
     runner.bestMlFlowRunId.toSeq.toDF("model_family", "run_id").createOrReplaceTempView("bestMlFlowRunId")
+
+    //Write out all the model to get them later in python
+
+    for(i <- runner.bestMlFlowRunId.keys){
+      val savePath = tmpPath + i.asInstanceOf[String]
+      runner.bestPipelineModel(i).write.overwrite().
+    }
+
+    def runInferencePipeline(mlFlowRunId:String,
+                            modelFamily:String,
+                            predictionType:String,
+                            labelCol:String,
+                            configs:Map[String,Any],
+                            df:DataFrame): Unit = {
+
+      // TO DO add support for default configs
+      // generate the configs
+      val configs = ConfigurationGenerator.generateConfigFromMap(modelFamily, predictionType, overrides)
+      // get logging config
+      val loggingConfig = configs.loggingConfig
+      // get pipeline model
+      val pipelineModel = PipelineModelInference.getPipelineModelByMlFlowRunId(mlFlowRunId, loggingConfig)
+      // run inference on df and pipeline model from mlflow
+      val inferenceDataFrame = pipelineModel.transform(df.drop("label")).createOrReplaceTempView("inference_df")
+      // store as temp view to get it back in python
+      inferenceDataFrame.createOrReplaceTempView("inferenceDF")
+    }
+
+
   }
 
   def buildArray(configs: Map[String, Any],
@@ -36,6 +67,7 @@ object FamilyRunnerUtil extends SparkSessionWrapper {
       })
       .toArray
   }
+
   def jsonToMap(message: String): Map[String, Any] = {
     objectMapper.registerModule(DefaultScalaModule)
     objectMapper.readValue(message, classOf[Map[String, Any]])
