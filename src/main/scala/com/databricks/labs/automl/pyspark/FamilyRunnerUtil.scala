f39c7251f50184749fcbdb1f3956866da74ccc23
srfnmnk <10840635+srfnmnk@users.noreply.github.com>
2020-03-18 07:41:46 -0400

    python types fixed

--- src/main/scala/com/databricks/labs/automl/pyspark/FamilyRunnerUtil.scala
+++ src/main/scala/com/databricks/labs/automl/pyspark/FamilyRunnerUtil.scala
@@ -24,13 +24,31 @@ object FamilyRunnerUtil extends SparkSessionWrapper {
     runner.bestMlFlowRunId.toSeq.toDF("model_family", "run_id").createOrReplaceTempView("bestMlFlowRunId")
   }
 
+  def cleansNestedTypes(valuesMap: Map[String, Any]): Map[String, Any] = {
+    val cleanMap: scala.collection.mutable.Map[String, Any] = scala.collection.mutable.Map()
+    if (valuesMap.contains("fieldsToIgnoreInVector")) {
+      cleanMap("fieldsToIgnoreInVector") = valuesMap("fieldsToIgnoreInVector").asInstanceOf[List[String]].toArray
+    }
+    if (valuesMap.contains("outlierFieldsToIgnore")) {
+      cleanMap("outlierFieldsToIgnore") = valuesMap("outlierFieldsToIgnore").asInstanceOf[List[String]].toArray
+    }
+    if (valuesMap.contains("numericBoundaries")) {
+      cleanMap("numericBoundaries") = valuesMap("numericBoundaries").asInstanceOf[Map[String, List[Any]]]
+        .flatMap({ case (k, v) => {
+          Map(k -> Tuple2(v.head.toString.toDouble, v(1).toString.toDouble))
+        }})
+    }
+    cleanMap.toMap
+  }
+
   def buildArray(configs: Map[String, Any],
                  predictionType: String): Array[InstanceConfig] = {
 
     configs
       .asInstanceOf[Map[String, Map[String, Any]]]
       .map({
-        case (key, valuesMap) => {
+        case (key, rawValuesMap) => {
+          val valuesMap: Map[String, Any] = rawValuesMap ++ cleansNestedTypes(rawValuesMap)
           ConfigurationGenerator.generateConfigFromMap(key, predictionType, valuesMap)
         }
       })
