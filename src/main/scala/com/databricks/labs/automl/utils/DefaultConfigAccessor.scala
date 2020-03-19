154654fc0afbf5a83334113a8c51e8281d9db532
benwilson <benjamin.wilson@databricks.com>
2020-03-19 16:41:01 -0400

    Adding default InstanceConfig getter

--- src/main/scala/com/databricks/labs/automl/utils/DefaultConfigAccessor.scala
+++ src/main/scala/com/databricks/labs/automl/utils/DefaultConfigAccessor.scala
@@ -1,11 +1,21 @@
 package com.databricks.labs.automl.utils
 
+import com.databricks.labs.automl.executor.config.{
+  ConfigurationDefaults,
+  InstanceConfig
+}
 import com.databricks.labs.automl.params.{Defaults, MainConfig}
 
 object DefaultConfigAccessor extends Defaults {
 
   def getMainConfig: MainConfig = _mainConfigDefaults
 
-  //TODO: finish this.
+}
+
+object DefaultInstanceConfigAccessor extends ConfigurationDefaults {
+
+  def getInstanceConfig(modelFamily: String,
+                        predictionType: String): InstanceConfig =
+    getDefaultConfig(modelFamily, predictionType)
 
 }
