b5644eca4809a198134ce8b5e9b9f1f174e9c10b
jas.bali <jas.bali@databricks.com>
2020-05-20 19:50:35 -0400

    Publish to maven central
    # Added a bash script to help publish to maven central

---
+++ publish-to-maven-central.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x
+
+OSSRH_USER=$1
+OSSRH_PASS=$2
+
+echo 'Starting Maven build and publish to maven central'
+mvn -V -B clean deploy -DskipTests -P "gpg-sign-deploy" -s settings.xml -Dossrh_user=OSSRH_USER -Dossrh_pass=OSSRH_PASS
+echo 'Finished Maven build and publish to maven central'
