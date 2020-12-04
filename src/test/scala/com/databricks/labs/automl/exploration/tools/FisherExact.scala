ef279d4cbe68f9880f97f8917b892aee7768b610
Nick.Senno <nick.senno@databricks.com>
2020-12-03 19:00:36 -0600

    removed Fisher Exact tests

--- src/test/scala/com/databricks/labs/automl/exploration/tools/FisherExact.scala
+++
@@ -1,7 +0,0 @@
-package com.databricks.labs.automl.exploration.tools
-
-import com.databricks.labs.automl.{AbstractUnitSpec, DiscreteTestDataGenerator}
-
-class FisherExact extends AbstractUnitSpec{
-
-}
